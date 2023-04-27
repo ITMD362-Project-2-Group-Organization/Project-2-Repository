e9e8bc02d7fe0c3ae1bf37cde9ec4f9ef46e124d
Michael Yelich <myelich@hawk.iit.edu>
2023-04-26 23:47:25 -0500

    navbar mobile menu button finished

--- js/script.js
+++ js/script.js
@@ -67,16 +67,12 @@ window.onclick = function(event) {
 function myFunction2() {
   document.getElementById("myDropdown2").classList.toggle("show");
 }
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+function myFunction4() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
+function myFunction5() {
+  document.getElementById("myDropdown5").classList.toggle("show");
 }
